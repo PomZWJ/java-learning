fa1258c5348f45fbbf8fdd8fbea40c1b9ac312ee
PomZWJ <1513041820@qq.com>
2021-03-25 23:21:02 +0800

    Initial commit

---
+++ springboot-rabbitmq/src/main/java/com/pomzwj/rabbitmq/RabbitMqApplication.java
@@ -0,0 +1,16 @@
+package com.pomzwj.rabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author PomZWJ
+ * @github https://github.com/PomZWJ
+ * @date 2021-03-25
+ */
+@SpringBootApplication
+public class RabbitMqApplication {
+    public static void main(String[] args){
+        SpringApplication.run(RabbitMqApplication.class);
+    }
+}
